b507527b34c248ff4ed3aeda8ec8286657d88b33
Andrew Blair <andrewjblair@gmail.com>
2022-02-15 22:42:01 -0500

    Implemented API to handle GET requests in ghg_data_server.py. Updated app route handlers to communicate with ghg data API.

--- ghg_data_server/ghg_data_server.py
+++ ghg_data_server/ghg_data_server.py
@@ -2,3 +2,24 @@
 # Date: 2/10/2022
 # Description: 
 
+from flask import Flask
+from flask_restful import Api, Resource
+import json
+
+app = Flask(__name__)
+api = Api(app)
+sourcePath = './ghg_data.json'
+
+class GhgData(Resource):
+    """Resource that can serve a greenhouse gas emissions data file."""
+    def get(self):
+        """Returns the contents of a JSON data file"""
+        with open(sourcePath, 'r') as dataFile:
+            data = json.load(dataFile)
+        return data
+        
+# Register GhgData resource with API at root endpoint
+api.add_resource(GhgData, '/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
